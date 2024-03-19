ad2fe1d19a7dc76a6de7f3fdadf8ef44b5a7234d
kristjansuligoj <kristjan.suligoj@aspiria.com>
2024-03-19 19:33:45 +0100

    Created "test_fetch_data.py" that tests the data fetch and added dependencies to pyproject.toml

---
+++ tests/test_fetch_data.py
@@ -0,0 +1,19 @@
+import pytest
+import requests
+
+
+def test_fetch_data():
+    url = 'https://api.jcdecaux.com/vls/v1/stations?contract=maribor&apiKey=5e150537116dbc1786ce5bec6975a8603286526b'
+
+    try:
+        # Send GET request
+        response = requests.get(url)
+
+        # Check if the request was successful
+        response.raise_for_status()
+
+    except requests.RequestException as e:
+        pytest.fail(f"Request failed: {e}")
+
+    # Assert that the status code is 200
+    assert response.status_code == 200, f"Unexpected status code: {response.status_code}. Response content: {response.content}"
