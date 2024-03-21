1cb1385c91897f87e762dea84da61584f2150e79
kristjansuligoj <kristjan.suligoj@aspiria.com>
2024-03-21 18:42:10 +0100

    api.py now runs on '0.0.0.0' instead of 'localhost', so it works when it is run as a docker image

--- src/serve/api.py
+++ src/serve/api.py
@@ -73,7 +73,7 @@ def main():
 
         return jsonify({'prediction': inversed_n_bike_stands.tolist()})
 
-    app.run(host='localhost', port=5000)
+    app.run(host='0.0.0.0', port=5000)
 
 
 if __name__ == "__main__":
