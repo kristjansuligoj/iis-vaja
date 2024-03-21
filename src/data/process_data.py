25062285c862357309f569f6fd8ef29e781cb030
kristjansuligoj <kristjan.suligoj@aspiria.com>
2024-03-21 21:14:38 +0100

    'process_data.py' now adds weather data to the raw data fetched by the api

--- src/data/process_data.py
+++ src/data/process_data.py
@@ -1,11 +1,12 @@
-import json
 import pandas as pd
+from common.common import save_df_to_csv
+import json
 
 
 def main():
     try:
         # Open the file in read mode
-        with open('../../data/raw/unprocessed_data.json', 'r') as f:
+        with open('../../data/raw/mbajk.json', 'r') as f:
             data = json.load(f)
 
         # Transform json to csv
@@ -24,15 +25,28 @@ def main():
         ]
         df = df[columns]
 
-        for station_name in df['name'].unique():
-            station_df = df[df['name'] == station_name].drop('name', axis=1)
+        # Fetch weather data
+        weather_df = pd.read_csv('../../data/raw/weather.csv')
 
-            # Transform UNIX ms to date time
-            station_df['date'] = pd.to_datetime(station_df['last_update'], unit='ms')
-            station_df = station_df.drop('last_update', axis=1)
+        # Transform date to same format bike_df date is, so merge can happen
+        weather_df['date'] = pd.to_datetime(weather_df['date']).dt.tz_localize(None)
 
-            processed_file_name = f'../../data/processed/processed_data_{station_name}.csv'
-            station_df.to_csv(processed_file_name, mode='a', index=False, header=False)
+        # Sort by last_update column, transform to UNIX datetime, then drop the last_update column
+        df.sort_values(by='last_update', inplace=True)
+        df['date'] = pd.to_datetime(df['last_update'], unit='ms')
+        df.drop('last_update', axis=1, inplace=True)
+
+        # Merge weather data with previous response
+        merged_df = pd.merge_asof(df, weather_df, left_on='date', right_on='date')
+
+        # Save a .csv file for each station
+        for station_name in merged_df['name'].unique():
+            # Drop unnecessary columns
+            station_df = merged_df[merged_df['name'] == station_name].drop('name', axis=1)
+
+            # Save the file
+            file_path = f'../../data/processed/processed_data_{station_name}.csv'
+            save_df_to_csv(file_path, station_df)
 
     except FileNotFoundError:
         print("File not found. Please make sure the file exists.")
