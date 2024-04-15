65370b332bc53d7950f6ac61495778ccaf2c24a3
kristjansuligoj <kristjan.suligoj@aspiria.com>
2024-04-15 18:12:17 +0200

    Updated "dvc.yaml"

--- src/data/fetch_mbajk_data.py
+++ src/data/fetch_mbajk_data.py
@@ -3,6 +3,7 @@ from definitions import ROOT_DIR
 
 import requests
 
+
 def main():
     url = 'https://api.jcdecaux.com/vls/v1/stations?contract=maribor&apiKey=5e150537116dbc1786ce5bec6975a8603286526b'
 
--- src/data/preprocess_weather_data.py
+++ src/data/preprocess_weather_data.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 from definitions import ROOT_DIR  # Assuming ROOT_DIR is defined in definitions.py
 
+
 def main():
     try:
         # Read all weather data
@@ -14,13 +15,16 @@ def main():
         # Read today's data
         today_file_path = os.path.join(ROOT_DIR, 'data', 'raw', 'weather', 'weather.csv')
         today_df = pd.read_csv(today_file_path)
-        today_df.to_csv(all_file_path, mode='a', header=False, index=False)
 
         # Filter yesterday's data
-        yesterday_df = all_df.loc[all_df['date'].dt.date == (datetime.now().date() - timedelta(days=1))]
+        yesterday_date = datetime.now().date() - timedelta(days=1)
+        yesterday_df = all_df.loc[all_df['date'].dt.date == yesterday_date]
 
         # Concatenate today and yesterday's data
         df = pd.concat([yesterday_df, today_df], ignore_index=True)
+
+        # Save concatenated data to all-weather.csv
+        df.to_csv(all_file_path, index=False)
 
         # Save to preprocessed file
         preprocessed_file_path = os.path.join(ROOT_DIR, 'data', 'raw', 'weather', 'preprocessed_weather.csv')
@@ -29,6 +33,7 @@ def main():
         print("File not found. Please make sure the file exists.")
     except IOError as e:
         print(f"An error occurred while processing the file: {e}")
+
 
 if __name__ == "__main__":
     main()
