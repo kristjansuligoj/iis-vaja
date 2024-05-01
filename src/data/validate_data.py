2b7cb34c0a363b202b81d3060078f335c4b9fa8b
kristjansuligoj <kristjan.suligoj@aspiria.com>
2024-05-01 13:07:44 +0200

    Updated validate_data.py

--- src/data/validate_data.py
+++ src/data/validate_data.py
@@ -6,7 +6,11 @@ import sys
 def main():
     data_context = great_expectations.DataContext(context_root_dir=(ROOT_DIR + "/gx"))
 
+    print("This is okay 1")
+
     validation = data_context.run_checkpoint(checkpoint_name="merged_data_checkpoint")
+
+    print("This is okay 2")
 
     if not validation["success"]:
         print("Validation failed!")
