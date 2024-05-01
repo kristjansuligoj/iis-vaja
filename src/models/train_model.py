9d14731138c6cba638a0ec7fbf2f8856024f01bb
kristjansuligoj <kristjan.suligoj@aspiria.com>
2024-05-01 19:20:41 +0200

    train_model.py now uses mlflow to track models

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,13 +1,20 @@
 import pandas as pd
 import numpy as np
+import tensorflow
+import dagshub
 import joblib
+import mlflow
 import os
 
-from sklearn.ensemble import BaggingRegressor
-from sklearn.preprocessing import MinMaxScaler, QuantileTransformer
 from sklearn.metrics import mean_absolute_error, mean_squared_error, explained_variance_score
+from sklearn.preprocessing import MinMaxScaler, QuantileTransformer
+from sklearn.ensemble import BaggingRegressor
+
+from common.common import make_directory_if_missing
 from definitions import ROOT_DIR
-import tensorflow
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def get_metrics(inverse_y_test, inverse_model_predictions):
@@ -67,7 +74,7 @@ def fill_empty_values(df):
     # Identify columns with missing values
     columns_with_missing_values = df.columns[df.isnull().any()].tolist()
     columns_with_complete_values = df.drop(
-        columns_with_missing_values + ["date"],
+        columns_with_missing_values + ["date"] + ["address"],
         axis=1
     ).columns.tolist()
 
@@ -99,24 +106,37 @@ def main():
     station_names = []
 
     for filename in os.listdir(station_directory):
-        if filename.endswith('.csv'):
+        if filename.startswith('processed_data='):
             # Parse station name from the file name
             station_name = filename.split('=')[1].split('.')[0]
             # Append station name to the list
             station_names.append(station_name)
 
+    dagshub.auth.add_app_token(token=os.getenv("DAGSHUB_TOKEN"))
+    dagshub.init(os.getenv("DAGSHUB_REPO_NAME"), os.getenv("DAGSHUB_USERNAME"), mlflow=True)
+    mlflow.set_tracking_uri(os.getenv("DAGSHUB_URI"))
+
     for station_name in station_names:
+        mlflow.start_run(run_name=f"run={station_name}")
+        mlflow.tensorflow.autolog()
+
         window_size = 24
+        batch_size = 32
+        epochs = 10
 
         # Read data
-        df = pd.read_csv(station_directory + 'processed_data=' + station_name + '.csv')
-        # df = pd.read_csv(ROOT_DIR + '/data/raw/mbajk/mbajk_dataset.csv')
+        df_train = pd.read_csv(station_directory + 'train_data=' + station_name + '.csv')
+        df_test = pd.read_csv(station_directory + 'test_data=' + station_name + '.csv')
 
         # Sort by date
-        df['date'] = pd.to_datetime(df['date'])
-        df = df.sort_values('date')
+        df_train['date'] = pd.to_datetime(df_train['date'])
+        df_test['date'] = pd.to_datetime(df_test['date'])
 
-        df = fill_empty_values(df)
+        df_train = df_train.sort_values('date')
+        df_test = df_test.sort_values('date')
+
+        df_train = fill_empty_values(df_train)
+        df_test = fill_empty_values(df_test)
 
         columns_of_interest = [
             'available_bike_stands',
@@ -130,24 +150,26 @@ def main():
             'bike_stands'
         ]
 
-        df_target_columns = df[columns_of_interest].values
+        df_train_target_columns = df_train[columns_of_interest].values
+        df_test_target_columns = df_test[columns_of_interest].values
         columns = columns_of_interest[1:]
 
         qt = QuantileTransformer(n_quantiles=32, output_distribution='normal')
 
         for column in columns:
-            array = np.array(df[column]).reshape(-1, 1)
-            df[column] = qt.fit_transform(array)
+            train_array = np.array(df_train[column]).reshape(-1, 1)
+            test_array = np.array(df_test[column]).reshape(-1, 1)
+
+            df_train[column] = qt.fit_transform(train_array)
+            df_test[column] = qt.fit_transform(test_array)
+
+        # Create directory for models if it doesn't exist
+        make_directory_if_missing(os.path.join(ROOT_DIR, "models", "transformers"))
 
         joblib.dump(qt, ROOT_DIR + '/models/transformers/transformer=' + station_name + '.pkl')
 
-        # Train length is all of available data
-        total_length = len(df_target_columns)
-        test_length = 100 + window_size
-        train_length = total_length - test_length
-
-        train_data = df_target_columns[:train_length]
-        test_data = df_target_columns[train_length:]
+        train_data = df_train_target_columns
+        test_data = df_test_target_columns
 
         abs_train_data = train_data[:, 0]
         abs_test_data = test_data[:, 0]
@@ -159,6 +181,8 @@ def main():
         other_scaler = MinMaxScaler()
         normalized_other_train_data = other_scaler.fit_transform(other_train_data)
         normalized_other_test_data = other_scaler.transform(other_test_data)
+
+        make_directory_if_missing(os.path.join(ROOT_DIR, "models", "scalers"))
 
         joblib.dump(
             other_scaler,
@@ -197,7 +221,10 @@ def main():
         gru_model = create_gru_model(input_shape)
 
         # Fit the model for specific staiton
-        gru_model.fit(X_train, y_train, epochs=30, validation_split=0.2)
+        gru_model.fit(X_train, y_train, epochs=epochs, validation_split=0.2, batch_size=batch_size)
+
+        mlflow.log_param("epochs", epochs)
+        mlflow.log_param("train_dataset_size", len(train_data))
 
         # Save the model for specific station
         gru_model.save(ROOT_DIR + '/models/model=' + station_name + '.h5')
@@ -205,6 +232,11 @@ def main():
         # Get metrics of TEST data
         gru_mae, gru_mse, gru_evs = get_model_metrics(gru_model, abs_scaler, X_test, y_test)
 
+        mlflow.log_metric("mae", gru_mae)
+        mlflow.log_metric("mse", gru_mse)
+        mlflow.log_metric("evs", gru_evs)
+
+        make_directory_if_missing(os.path.join(ROOT_DIR, "reports"))
         with open(ROOT_DIR + '/reports/model=' + station_name + '-metrics.txt', 'w', encoding='utf-8') as f:
             f.write(
                 f'Mean average error: {gru_mae}\nMean square error: {gru_mse}\nExplained variance score: {gru_evs}\n')
@@ -215,6 +247,8 @@ def main():
         with open(ROOT_DIR + '/reports/model=' + station_name + '-train_metrics.txt', 'w', encoding='utf-8') as f:
             f.write(
                 f'Mean average error: {gru_mae}\nMean square error: {gru_mse}\nExplained variance score: {gru_evs}\n')
+
+        mlflow.end_run()
 
 
 if __name__ == "__main__":
