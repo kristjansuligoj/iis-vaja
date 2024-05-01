6ced6b8b00c09d1fbc7d7971f69ab5da62405f16
kristjansuligoj <kristjan.suligoj@aspiria.com>
2024-05-01 11:51:47 +0200

    Updated gx data context, suite and checkpoint

--- src/data/validate_data.py
+++ src/data/validate_data.py
@@ -5,7 +5,7 @@ import great_expectations
 def main():
     data_context = great_expectations.DataContext(context_root_dir=ROOT_DIR + "/gx")
 
-    validation = data_context.run_checkpoint(checkpoint_name="new_data_checkpoint", batch_request=None, run_name=None)
+    validation = data_context.run_checkpoint(checkpoint_name="merged_data_checkpoint", batch_request=None, run_name=None)
 
     if validation["success"]:
         print("Validation succeeded!")
