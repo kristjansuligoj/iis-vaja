af09cca1d860e4868c0705b37aa0076df0ce59a6
kristjansuligoj <kristjan.suligoj@aspiria.com>
2024-05-01 13:19:51 +0200

    Moved validate_data.py to gx directory

--- gx/validate_data.py
+++ gx/validate_data.py
@@ -1,14 +1,9 @@
-from definitions import ROOT_DIR
 import great_expectations
 import sys
 
 
 def main():
-    try:
-        data_context = great_expectations.DataContext(context_root_dir=(ROOT_DIR + "/gx"))
-    except great_expectations.exceptions.DataContextError as e:
-        print(f"Error loading data context: {e}")
-        sys.exit(1)
+    data_context = great_expectations.DataContext()
 
     validation = data_context.run_checkpoint(
         checkpoint_name="merged_data_checkpoint",
