60a49b105112aa9ca4e1768cf8d930cf656868a5
kristjansuligoj <kristjan.suligoj@aspiria.com>
2024-05-03 15:33:48 +0200

    Updated 'download_scaler' function

--- src/models/mlflow_client.py
+++ src/models/mlflow_client.py
@@ -26,12 +26,10 @@ def download_scaler(station_name, scaler_type, stage):
         client = mlflow.MlflowClient()
 
         # Get scaler latest staging source
-        latest_scaler_source = client.get_latest_versions(name=scaler_name, stages=[stage])
-
-        print(latest_scaler_source)
+        latest_scaler_source = client.get_latest_versions(name=scaler_name, stages=[stage])[0].source
 
         # Load the scaler by its source
-        return mlflow.sklearn.load_model(latest_scaler_source[0].source)
+        return mlflow.sklearn.load_model(latest_scaler_source)
     except IndexError:
         print(f"There was an error downloading {scaler_name} in {stage}")
         return None
